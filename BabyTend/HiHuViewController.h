f778436c664ee6bd503f384be7fad529fffebbf3
benhi <505128283@qq.com>
2014-05-05 17:14:53 +0800

    签到属性

--- BabyTend/HiHuViewController.h
+++ BabyTend/HiHuViewController.h
@@ -18,6 +18,10 @@
 @property (strong, nonatomic) UIButton *babyName;
 @property (strong, nonatomic) UIButton *timeLabel;
 @property (strong, nonatomic) UIButton *health;
+@property (weak, nonatomic) IBOutlet UILabel *weekDay;
+@property (weak, nonatomic) IBOutlet UIImageView *signOrNot;
+@property (weak, nonatomic) IBOutlet UILabel *signTime;
+@property (weak, nonatomic) IBOutlet UILabel *signBaby;
 
 @property (weak, nonatomic) IBOutlet UILabel *telephoneNumber;
 @end
